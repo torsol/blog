b6b5e2c51f22c553b8454e82f058748ec1279453
Torstein Solberg <soltors@gmail.com>
2020-08-31 16:40:05 +0200

    Updates

---
+++ component---src-pages-kontaktinfo-js-bce42dfe15157c74963f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{yhb1:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("6Gk8"),r=a("Bl7J"),c=a("vrFN");t.default=function(e){e.data;var t=e.location;return l.a.createElement(r.a,{location:t,title:"Kontaktinfo"},l.a.createElement("h2",null,"Mer info kommer.."),l.a.createElement(c.a,{title:"Om meg"}),l.a.createElement(o.a,null))}}}]);
+//# sourceMappingURL=component---src-pages-kontaktinfo-js-bce42dfe15157c74963f.js.map
